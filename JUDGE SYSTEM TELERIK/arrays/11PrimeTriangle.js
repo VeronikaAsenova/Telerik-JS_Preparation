1a234e68c33ccbbc146e5f2b670bcb569be78991
Veronika Asenova <veronikaasenova0@gmail.com>
2019-01-10 15:06:36 +0200

    Update 11PrimeTriangle.js

--- JUDGE SYSTEM TELERIK/arrays/11PrimeTriangle.js
+++ JUDGE SYSTEM TELERIK/arrays/11PrimeTriangle.js
@@ -12,17 +12,25 @@ const getGets = (arr) => {
 // this is the test
 const test = [
     '10'
-
 ];
 
 const gets = this.gets || getGets(test);
 const print = this.print || console.log;
 
-const n = gets().split().map(el => +el);
+const n = +gets();
 let array = [];
 let prime = [];
+let not = [];
 
 function isPrime(n) {
+    let flagIsPrime = true;
+    for (let index = 2; index < n; index++) {
+        if (n % index === 0) {
+            flagIsPrime = false;
+            break;
+        }
+    }
+    return flagIsPrime;
 }
 
 //fill the array from 1 to N;
@@ -30,21 +38,26 @@ for (let index = 1; index <= n; index++) {
     array.push(index);
 }
 
-//here is how many rows we will p
+// console.log(array);
+
+// //here is how many rows we will p
 for (let j = 0; j < array.length; j++) {
-    if (isNaN(array[j])) {
-        continue;
-    } else {
-        if (isPrime(array[j])) {
-            prime.push(array[[j]]);
-        }
+
+    if (isPrime(array[j])) {
+        prime.push(array[[j]]);
     }
+
 }
 
+// console.log(prime);
 
+for(let i = 0; i < prime.length; i++) {
+    const primeNumber = prime[i];
+    const numberRow = [];
 
-
-print(array);
-print(prime);
-
+    for(let j = 1; j <= primeNumber; j++) {
+        const numberToPrint = isPrime(j) ? 1 : 0;
+        numberRow.push(numberToPrint);
+    }
+    print(numberRow.join(''));
 
